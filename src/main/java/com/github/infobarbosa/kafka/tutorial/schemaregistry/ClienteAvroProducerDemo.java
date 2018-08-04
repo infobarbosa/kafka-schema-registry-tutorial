d06e37404dc6efd7bf07b615380a05c84d9f2fc1
Marcelo Barbosa Pinto <infobarbosa@yahoo.com.br>
2018-08-04 11:58:52 -0300

    refactoring. renomeando Main para ClienteAvroProducerDemo

--- src/main/java/com/github/infobarbosa/kafka/tutorial/schemaregistry/ClienteAvroProducerDemo.java
+++ src/main/java/com/github/infobarbosa/kafka/tutorial/schemaregistry/ClienteAvroProducerDemo.java
@@ -7,7 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
-public class ClienteDemo {
+public class ClienteAvroProducerDemo {
     public static void main(String[] args) {
 
         Properties properties = new Properties();
