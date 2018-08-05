9b5a10755609773fec819b480170df1991c1d8b0
Marcelo Barbosa Pinto <infobarbosa@yahoo.com.br>
2018-08-05 10:41:15 -0300

    refactoring da classe de conexao para uma classe factory

--- src/main/java/com/github/infobarbosa/kafka/tutorial/schemaregistry/ClienteAvroProducerDemo.java
+++ src/main/java/com/github/infobarbosa/kafka/tutorial/schemaregistry/ClienteAvroProducerDemo.java
@@ -1,23 +1,13 @@
 package com.github.infobarbosa.kafka.tutorial.schemaregistry;
 
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import com.github.infobarbosa.kafka.tutorial.schemaregistry.streamfactory.KafkaProducerFactory;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
 
 public class ClienteAvroProducerDemo {
     public static void main(String[] args) {
 
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-        properties.setProperty("acks", "all");
-        properties.setProperty("retries", "10");
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", KafkaAvroSerializer.class.getName());
-        properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
 
-        Producer<String, Cliente> producer = new KafkaProducer<String, Cliente>(properties);
+        Producer<String, Cliente> producer = KafkaProducerFactory.getInstance();
 
         String topic = "cliente";
 
