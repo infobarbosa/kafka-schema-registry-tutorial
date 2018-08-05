2b961c04947b261001458ef1b600a390856955ca
Marcelo Barbosa Pinto <infobarbosa@yahoo.com.br>
2018-08-05 10:03:59 -0300

    adicionando propriedades ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG e ConsumerConfig.AUTO_OFFSET_RESET_CONFIG

--- src/main/java/com/github/infobarbosa/kafka/tutorial/schemaregistry/ClienteAvroConsumerDemo.java
+++ src/main/java/com/github/infobarbosa/kafka/tutorial/schemaregistry/ClienteAvroConsumerDemo.java
@@ -14,8 +14,8 @@ public class ClienteAvroConsumerDemo {
     public static void main(String[] args) {
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        //properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        //properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "cliente-consumer");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
