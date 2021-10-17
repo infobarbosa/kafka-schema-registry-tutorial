c757f29638aae0ff2b4b0edcb9b9066d7633e16b
infobarbosa <infobarbosa@yahoo.com.br>
2021-10-17 20:54:51 -0300

    adicionando os fontes para tratamento do evento Pagamento Aprovado.

--- kafka-avro-producer/src/main/java/com/github/infobarbosa/kafka/AvroProducerTutorial.java
+++ kafka-avro-producer/src/main/java/com/github/infobarbosa/kafka/AvroProducerTutorial.java
@@ -3,45 +3,69 @@ package com.github.infobarbosa.kafka;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.UUID;
 
 public class AvroProducerTutorial {
+    private final static Logger logger = LoggerFactory.getLogger(AvroProducerTutorial.class);
+
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
         properties.setProperty("acks", "all");
         properties.setProperty("retries", "10");
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", KafkaAvroSerializer.class.getName());
-        properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+        properties.setProperty("schema.registry.url", System.getenv("SCHEMA_REGISTRY_URL"));
 
-        Producer<String, Cliente> producer = new KafkaProducer<>(properties);
+        Producer<String, PagamentoAprovado> producer = new KafkaProducer<>(properties);
 
-        String topic = "cliente";
+        PagamentoAprovado pagamento = null;
+        String topic = "pagamento-aprovado";
 
-        Cliente cliente  = Cliente.newBuilder()
-                .setCpf("99988877742")
-                .setId("1L")
-                .setPrimeiroNome("Marcelo")
-                .setSobrenome("Barbosa")
+        for(int i=0; i < 100000; i++){
+            UUID idPagamento = UUID.randomUUID();
+            UUID idPedido = UUID.randomUUID();
+
+            pagamento = PagamentoAprovado.newBuilder()
+                .setId(idPagamento.toString())
+                .setIdPedido(idPedido.toString())
+                .setValor("10.00")
+                .setData(System.currentTimeMillis())
                 .build();
 
-        ProducerRecord<String, Cliente> producerRecord = new ProducerRecord<String, Cliente>(
-                topic, cliente
-        );
+            ProducerRecord<String, PagamentoAprovado> producerRecord = 
+                new ProducerRecord<String, PagamentoAprovado>(topic, idPagamento.toString(), pagamento);
 
-        System.out.println(cliente);
-        producer.send(producerRecord, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                if (exception == null) {
-                    System.out.println(metadata.partition());
-                } else {
-                    exception.printStackTrace();
+            logger.info(pagamento.toString());
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception == null) {
+                        logger.info(
+                            "K: " + idPagamento.toString() +
+                            ". P: " + metadata.partition() +
+                            ". OS: " + metadata.offset() +
+                            ". TS: " + metadata.timestamp()
+                        );
+                    } else {
+                        exception.printStackTrace();
+                    }
                 }
+            });
+
+            //coloca pra dormir um pouco
+            try {
+                Thread.sleep(100);
             }
-        });
+            catch(InterruptedException e){
+                logger.error("problemas durante o sono.", e);
+            }
+
+        }
 
         producer.flush();
         producer.close();
